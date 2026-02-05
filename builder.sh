1bffe2b6f4487fbeb809dcec62093b91eeb3b2d7
Caleb Pierce <calebrpierce@gmail.com>
2026-02-05 15:31:50 -0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,6 +2,8 @@
 python3 -m venv ~/Library/env
 source ~/Library/env/bin/activate
 pip install --upgrade pip
-pip install spidev mfrc522
-cp Library-IMS/Library.desktop ~/Desktop/Library.desktop
-chmod u+x ~/Desktop/Library.desktop
+pip install spidev mfrc522 snipeit
+cp Library-IMS/Library.Desktop ~/Desktop/Library.Desktop
+chmod u+x ~/Desktop/Library.Desktop
+mv ~/Library/Library-IMS/Assets.py ~/Library/env/lib/python3.13/site-packages/snipeit/
+mv ~/Library/Library-IMS/Users.py ~/Library/env/lib/python3.13/site-packages/snipeit/
